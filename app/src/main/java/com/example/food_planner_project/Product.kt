b3b4362c4244a423219bb750509a2206e8ee01e9
Maks <6610257+Xpym482@users.noreply.github.com>
2020-06-16 17:12:47 +0300

    refactor Product.kt

--- app/src/main/java/com/example/food_planner_project/Product.kt
+++ app/src/main/java/com/example/food_planner_project/Product.kt
@@ -1,7 +1,5 @@
 package com.example.food_planner_project
 
-import android.os.Parcel
-import android.os.Parcelable
 import java.io.Serializable
 
 data class Product (val id: Long = 0, val title : String = "", var Rasvad : Int = 0, var Susivesikud : Int = 0, var Valgud : Int = 0, var Kalorid : Int = 0) : Serializable
